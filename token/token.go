b1a2bf5392af38845982e551b9815ea6c75c9dc6
chtka <chtka.ow@gmail.com>
2019-06-21 22:04:14 -0700

    Initial commit.

---
+++ token/token.go
@@ -0,0 +1,72 @@
+// Package token provides a tokenizer for the Monkey
+// programming language.
+package token
+
+// TokenType indicates what type a Token has.
+type TokenType string
+
+const (
+	ILLEGAL TokenType = "ILLEGAL"
+	EOF     TokenType = "EOF"
+
+	// Identifiers + literals
+	IDENT TokenType = "IDENT"
+	INT   TokenType = "INT"
+
+	// Operators
+	ASSIGN   TokenType = "="
+	PLUS     TokenType = "+"
+	MINUS    TokenType = "-"
+	BANG     TokenType = "!"
+	ASTERISK TokenType = "*"
+	SLASH    TokenType = "/"
+
+	LT TokenType = "<"
+	GT TokenType = ">"
+	EQ TokenType = "=="
+	NOT_EQ TokenType = "!="
+
+	// Delimiters
+	COMMA     TokenType = ","
+	SEMICOLON TokenType = ";"
+
+	LPAREN TokenType = "("
+	RPAREN TokenType = ")"
+	LBRACE TokenType = "{"
+	RBRACE TokenType = "}"
+
+	// Keywords
+	FUNCTION TokenType = "FUNCTION"
+	LET      TokenType = "LET"
+	TRUE     TokenType = "TRUE"
+	FALSE    TokenType = "FALSE"
+	IF       TokenType = "IF"
+	ELSE     TokenType = "ELSE"
+	RETURN   TokenType = "RETURN"
+)
+
+// Token represents a Monkey language token in the
+// lexical analysis step.
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
+}
+
+// LookupIdent checks the keywords table to check whether
+// the given identifier is a keyword.
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
